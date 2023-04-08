c098b257779e8f3e72261fabc7c1bc636ae276bc
Davi Samuel Da Silva <davidev.front@gmail.com>
2023-04-08 13:44:40 -0300

    [M1S10] Ex 1 - Criando o projeto e adicionando as dependências

---
+++ Formulário Multi Passos/src/App.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+// import DadosPessoais from './DadosPessoais';
+// import Endereco from './Endereco';
+// import Agradecimento from './Agradecimento';
+
+function App() {
+  const [passoAtual, setPassoAtual] = useState(0);
+
+  return (
+    <div className="app">
+      <div>
+        <form>
+          {passoAtual === 0 && <DadosPessoais />}
+          {passoAtual === 1 && <Endereco />}
+          {passoAtual === 2 && <Agradecimento />}
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
